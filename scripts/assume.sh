5e5746b1d38e3252ec33e8d53a2c18348040b51b
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2018-06-29 15:57:03 +0900

    test

--- scripts/assume.sh
+++ scripts/assume.sh
@@ -10,6 +10,8 @@ aws_sts_credentials="$(aws sts assume-role \
   --query "Credentials" \
   --output "json")"
 
+cat <<EOT > "aws_sts_env.sh"
 export AWS_ACCESS_KEY_ID="$(echo $aws_sts_credentials | jq -r '.AccessKeyId')"
 export AWS_SECRET_ACCESS_KEY="$(echo $aws_sts_credentials | jq -r '.SecretAccessKey')"
 export AWS_SESSION_TOKEN="$(echo $aws_sts_credentials | jq -r '.SessionToken')"
+EOT
